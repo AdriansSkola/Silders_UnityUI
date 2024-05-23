ef0140dea02f89469ac7159eb7e7a6f5045db904
AdriansSkola <adrians.silders@sk.lvt.lv>
2024-05-24 00:32:27 +0300

    Pievienoju sliders apģērbu izmēru mainīšanai, kā arī nomainiju un pieliku skaņas

--- Assets/Script/ImageScript.cs
+++ Assets/Script/ImageScript.cs
@@ -27,6 +27,20 @@ public class ImageScript : MonoBehaviour
     public GameObject CharHolder;
     public Sprite[] SpriteArray;
 
+    public Slider HeightHair;
+    public Slider WidthHair;
+    public Slider HeightShirt;
+    public Slider WidthShirt;
+    public Slider HeightPants;
+    public Slider WidthPants;
+    public Slider HeightBoards;
+    public Slider WidthBoards;
+
+    public GameObject[] hairObjects;
+    public GameObject[] shirtObjects;
+    public GameObject[] pantsObjects;
+    public GameObject[] boardsObjects;
+
     public void showHair(bool value)
     {
         hair1.SetActive(value);
@@ -61,5 +75,49 @@ public class ImageScript : MonoBehaviour
     public void changeImage(int index)
     {
         CharHolder.GetComponent<Image>().sprite = SpriteArray[index];
+    }
+
+    public void ChangeSizeHair()
+    {
+        float hairSizeH = HeightHair.value;
+        float hairSizeW = WidthHair.value;
+
+        foreach (GameObject hair in hairObjects)
+        {
+            hair.transform.localScale = new Vector3(1.0f * hairSizeH, 1.0f * hairSizeW, 1);
+        }
+    }
+
+    public void ChangeSizeShirt()
+    {
+        float shirtSizeH = HeightShirt.value;
+        float shirtSizeW = WidthShirt.value;
+
+        foreach (GameObject shirt in shirtObjects)
+        {
+            shirt.transform.localScale = new Vector3(1.0f * shirtSizeH, 1.0f * shirtSizeW, 1);
+        }
+    }
+
+    public void ChangeSizePants()
+    {
+        float pantsSizeH = HeightPants.value;
+        float pantsSizeW = WidthPants.value;
+
+        foreach (GameObject pants in pantsObjects)
+        {
+            pants.transform.localScale = new Vector3(1.0f * pantsSizeH, 1.0f * pantsSizeW, 1);
+        }
+    }
+
+    public void ChangeSizeBoards()
+    {
+        float boardSizeH = HeightBoards.value;
+        float boardSizeW = WidthBoards.value;
+
+        foreach (GameObject board in boardsObjects)
+        {
+            board.transform.localScale = new Vector3(1.0f * boardSizeH, 1.0f * boardSizeW, 1);
+        }
     }
 }
